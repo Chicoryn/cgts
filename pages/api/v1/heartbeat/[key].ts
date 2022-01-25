ba25eecbfdb4469701615662148c971e8793ec6b
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-25 03:50:43 +0000

    Heartbeat API work

--- pages/api/v1/heartbeat/[key].ts
+++ pages/api/v1/heartbeat/[key].ts
@@ -15,21 +15,38 @@ export default async function handler(
 )
 {
   if (req.method === 'GET') {
-    const new_hook_key = randomBytes(8).toString('base64url');
     const engine = await prisma.engine.findUnique({
-      where: { key: <string>req.query.key }
+      where: { key: <string>req.query.key },
+      include: {
+        participating: {
+          where: { game: { active: true } },
+          include: {
+            game: true
+          }
+        }
+      }
     });
 
     if (!engine) {
       res.status(404).json({
         error: 'no such engine'
       })
+    } else if (!engine.participating.length) {
+      res.status(204).json({});
     } else {
-      res.status(200).json({
-        sequence: [],
-        action: 'genmove b',
-        webhook_url: `api/v1/hook/${new_hook_key}`
-      })
+      const new_hook_key = randomBytes(8).toString('base64url');
+      const participation = engine.participating[0];
+      const sequence = <Array<string>>participation.game.sequence;
+
+      if (sequence[-1].startsWith(participation.color)) {
+        res.status(204).json({});
+      } else {
+        res.status(200).json({
+          sequence: sequence,
+          action: `genmove ${participation.color}`,
+          webhook_url: `api/v1/hook/${new_hook_key}`
+        })
+      }
     }
   } else {
     res.status(400).json({ error: 'bad request' })
