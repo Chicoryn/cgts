a563308a3786da786604236c0e9e0ada793c67d9
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-25 04:09:55 +0000

    Webhook API work

--- pages/api/v1/hook/[key].ts
+++ pages/api/v1/hook/[key].ts
@@ -1,4 +1,5 @@
 import prisma from '../../../../lib/db';
+import { Prisma } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -11,9 +12,32 @@ export default async function handler(
 )
 {
   if (req.method === 'POST') {
-    const key = req.body;
+    const participant = await prisma.participant.findUnique({
+      where: {
+        key: <string>req.query.key
+      },
+      include: {
+        game: true,
+        engine: true
+      }
+    });
 
-    res.status(200).json({})
+    if (!participant) {
+      res.status(404).json({error: 'no such webhook'})
+    } else {
+      const color = participant.color;
+      const body = JSON.parse(req.body);
+      const response = body['response'];
+
+      if (!response) {
+        res.status(400).json({error: 'bad request'})
+      } else {
+        let sequence = participant.game.sequence as Prisma.JsonArray;
+        sequence.push(`${color} ${response}`);
+
+        res.status(200).json({})
+      }
+    }
   } else {
     res.status(400).json({error: 'bad request'})
   }
