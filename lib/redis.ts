72db5a07982eb0e3423869dcec37e8fab2d558d7
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-27 01:27:15 +0000

    Clean up the duplicate redis connection once we're done

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,10 +4,16 @@ import Redis from 'ioredis';
 const redis = new Redis(process.env.REDIS_URL);
 
 export async function wakeUp(engine: Engine) {
-    let pipeline = redis.duplicate().pipeline();
-    pipeline.lpush(`wake:${engine.id}`, '1');
-    pipeline.expire(`wake:${engine.id}`, 3);
-    await pipeline.exec();
+    const duplicate = redis.duplicate();
+
+    try {
+        let pipeline = duplicate.pipeline();
+        pipeline.lpush(`wake:${engine.id}`, '1');
+        pipeline.expire(`wake:${engine.id}`, 3);
+        await pipeline.exec();
+    } finally {
+        duplicate.disconnect();
+    }
 }
 
 export async function sleepUntilWake(engine: Engine, timeout: number) {
