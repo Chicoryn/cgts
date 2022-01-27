7cb003af2709cf79247affb410b35d22dc336482
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2022-01-27 01:19:47 +0000

    Use separate redis connections for pub/sub ends

--- lib/redis.ts
+++ lib/redis.ts
@@ -4,7 +4,7 @@ import Redis from 'ioredis';
 const redis = new Redis(process.env.REDIS_URL);
 
 export async function wakeUp(engine: Engine) {
-    let pipeline = redis.pipeline();
+    let pipeline = redis.duplicate().pipeline();
     pipeline.lpush(`wake:${engine.id}`, '1');
     pipeline.expire(`wake:${engine.id}`, 3);
     await pipeline.exec();
